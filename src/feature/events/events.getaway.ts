a580e22aa499f80638bca0e25e7a2318e0fa324b
nhatvu1998 <nhatvu28998@gmail.com>
2020-09-22 19:18:40 +0700

    add video call functionality

--- src/feature/events/events.getaway.ts
+++ src/feature/events/events.getaway.ts
@@ -6,32 +6,34 @@ import {
   WsResponse,
   ConnectedSocket
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Client, Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { UserEntity } from '../user/entity/user.entity';
 
 const rooms = {};
 
 @WebSocketGateway()
-export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  constructor() {
-    const rooms = {};
-  }
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly userService: UserService,
+  ) {}
+
   @WebSocketServer() server: Server;
   private logger = new Logger('Events Gateway')
-  afterInit(server: Server) {
-    this.logger.log('Init');
+
+  async handleConnection(socket) {
+    const user = await this.userService.decodeToken(socket.handshake.query.token)
+    console.log(user);
+    this.logger.log(`Client connected: ${socket.id}`);
   }
 
   handleDisconnect(client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
-    this.logger.log(`Client connected: ${client.id}`);
-  }
-
   @SubscribeMessage('join room')
-  findAll(@MessageBody() roomID: any, client: Socket): Promise<unknown> {
+  findAll(client: Socket, roomID: any): Promise<unknown> {
     if (rooms[roomID]) {
       rooms[roomID].push(client.id);
     } else {
@@ -42,25 +44,26 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
       client.emit('other user', otherUser);
       client.to(otherUser).emit('user joined', client.id);
     }
+    client.emit('roomId', roomID);
     return roomID;
   }
 
   @SubscribeMessage('offer')
-  offer(@MessageBody() payload: any, @ConnectedSocket() client: Socket,): Promise<unknown> {
+  offer(@MessageBody() payload: any, @ConnectedSocket() client: Socket): Promise<unknown> {
     console.log(payload);
     client.to(payload.target).emit('offer', payload)
     return payload;
   }
 
   @SubscribeMessage('answer')
-  answer(@MessageBody() payload: any, @ConnectedSocket() client: Socket,): Promise<unknown> {
+  answer(@MessageBody() payload: any, @ConnectedSocket() client: Socket): Promise<unknown> {
     console.log(payload);
     client.to(payload.target).emit('answer', payload)
     return payload;
   }
 
   @SubscribeMessage('ice-candidate')
-  iceCandidate(@MessageBody() incoming: any, @ConnectedSocket() client: Socket,): Promise<unknown> {
+  iceCandidate(@MessageBody() incoming: any, @ConnectedSocket() client: Socket): Promise<unknown> {
     console.log(incoming);
     client.to(incoming.target).emit('ice-candidate', incoming.candidate)
     return incoming;
--- src/feature/events/events.module.ts
+++ src/feature/events/events.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.getaway';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [EventsGateway],
 })
 export class EventsModule {}
