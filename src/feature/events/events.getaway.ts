43a36be92c4ecf4f714ca9992d826efbf0bb3681
nhatvu1998 <nhatvu28998@gmail.com>
2020-10-01 18:00:17 +0700

    1

--- src/feature/events/events.getaway.ts
+++ src/feature/events/events.getaway.ts
@@ -9,7 +9,6 @@ import {
 import { Client, Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import { UserService } from '../user/user.service';
-import { UserEntity } from '../user/entity/user.entity';
 
 const rooms = {};
 
@@ -24,7 +23,6 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   async handleConnection(socket) {
     const user = await this.userService.decodeToken(socket.handshake.query.token)
-    console.log(user);
     this.logger.log(`Client connected: ${socket.id}`);
   }
 
@@ -32,39 +30,46 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
+  // @SubscribeMessage('call signal')
+  // callSignal(@MessageBody() roomId: any, @ConnectedSocket() client: Socket): Promise<unknown> {
+  //   const otherUser = rooms[roomId].find(id => id !== client.id);
+  //   if (otherUser) {
+  //     client.emit('other user', otherUser);
+  //     client.to(otherUser).emit('user joined', client.id);
+  //   }
+  // }
+
   @SubscribeMessage('join room')
-  findAll(client: Socket, roomID: any): Promise<unknown> {
-    if (rooms[roomID]) {
-      rooms[roomID].push(client.id);
+  findAll(client: Socket, roomId: any): Promise<unknown> {
+    if (rooms[roomId]) {
+      rooms[roomId].push(client.id);
     } else {
-      rooms[roomID] = [client.id];
+      rooms[roomId] = [client.id];
     }
-    const otherUser = rooms[roomID].find(id => id !== client.id);
+    const otherUser = rooms[roomId].find(id => id !== client.id);
     if (otherUser) {
       client.emit('other user', otherUser);
       client.to(otherUser).emit('user joined', client.id);
     }
-    client.emit('roomId', roomID);
-    return roomID;
+    console.log(roomId);
+    client.emit('roomId', roomId);
+    return roomId;
   }
 
   @SubscribeMessage('offer')
   offer(@MessageBody() payload: any, @ConnectedSocket() client: Socket): Promise<unknown> {
-    console.log(payload);
     client.to(payload.target).emit('offer', payload)
     return payload;
   }
 
   @SubscribeMessage('answer')
   answer(@MessageBody() payload: any, @ConnectedSocket() client: Socket): Promise<unknown> {
-    console.log(payload);
     client.to(payload.target).emit('answer', payload)
     return payload;
   }
 
   @SubscribeMessage('ice-candidate')
   iceCandidate(@MessageBody() incoming: any, @ConnectedSocket() client: Socket): Promise<unknown> {
-    console.log(incoming);
     client.to(incoming.target).emit('ice-candidate', incoming.candidate)
     return incoming;
   }
